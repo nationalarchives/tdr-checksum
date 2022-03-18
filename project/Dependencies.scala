49c629f3e74220a888e0a1ab2142ca910e4fa37b
Scala Steward <me@scala-steward.org>
2022-03-18 23:12:04 +0100

    Update cats-effect to 2.5.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   lazy val circeGeneric = "io.circe" %% "circe-generic" % "0.13.0"
   lazy val circeParser = "io.circe" %% "circe-parser" % "0.13.0"
   lazy val awsUtils =  "uk.gov.nationalarchives.aws.utils" %% "tdr-aws-utils" % "0.1.16"
-  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "2.2.0"
+  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "2.5.4"
   lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
   lazy val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
   lazy val logstashLogbackEncoder = "net.logstash.logback" % "logstash-logback-encoder" % "6.6"
