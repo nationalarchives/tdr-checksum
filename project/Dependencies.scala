58e5d53102687523c5a700bc4e52b69d1f9865e7
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-04-25 08:23:23 +0000

    Update scalatest to 3.2.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   lazy val lambdaJavaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.11.0"
   lazy val generatedGraphql = "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.234"
   lazy val typesafe = "com.typesafe" % "config" % "1.4.2"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.11"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.12"
   lazy val elasticMq = "org.elasticmq" %% "elasticmq-server" % "1.3.7"
   lazy val elasticMqSqs = "org.elasticmq" %% "elasticmq-rest-sqs" % "1.3.7"
   lazy val circeCore = "io.circe" %% "circe-core" % "0.14.1"
