80f81ea9323fde1d0d9bb63149b8a9c5582015a6
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-11-01 08:25:32 +0000

    Update tdr-aws-utils to 0.1.46

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   lazy val circeCore = "io.circe" %% "circe-core" % "0.14.3"
   lazy val circeGeneric = "io.circe" %% "circe-generic" % "0.14.3"
   lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.3"
-  lazy val awsUtils =  "uk.gov.nationalarchives" %% "tdr-aws-utils" % "0.1.44"
+  lazy val awsUtils =  "uk.gov.nationalarchives" %% "tdr-aws-utils" % "0.1.46"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.3.14"
   lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5"
   lazy val logback = "ch.qos.logback" % "logback-classic" % "1.4.4"
