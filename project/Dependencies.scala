d9f0da833c667459cbb445de9a8a2d615375c52f
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-04-22 08:25:17 +0000

    Update s3-utils to 0.1.81

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,6 +7,6 @@ object Dependencies {
   lazy val circeGeneric = "io.circe" %% "circe-generic" % "0.14.5"
   lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.5"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.4.9"
-  lazy val s3Utils = "uk.gov.nationalarchives" %% "s3-utils" % "0.1.80"
+  lazy val s3Utils = "uk.gov.nationalarchives" %% "s3-utils" % "0.1.81"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "2.27.2"
 }
