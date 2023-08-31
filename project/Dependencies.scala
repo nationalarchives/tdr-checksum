b3221ba90277f34a1fa10c7574fdeacd74edf6d4
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-08-31 08:26:26 +0000

    Update circe-core, circe-generic, ... to 0.14.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,9 +3,9 @@ import sbt._
 object Dependencies {
   lazy val typesafe = "com.typesafe" % "config" % "1.4.2"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16"
-  lazy val circeCore = "io.circe" %% "circe-core" % "0.14.5"
-  lazy val circeGeneric = "io.circe" %% "circe-generic" % "0.14.5"
-  lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.5"
+  lazy val circeCore = "io.circe" %% "circe-core" % "0.14.6"
+  lazy val circeGeneric = "io.circe" %% "circe-generic" % "0.14.6"
+  lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.6"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.1"
   lazy val s3Utils = "uk.gov.nationalarchives" %% "s3-utils" % "0.1.97"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "2.27.2"
