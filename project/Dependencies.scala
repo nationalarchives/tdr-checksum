11baeae1d173b0674ba98130a57aa519e9087181
TomJKing <tom.king@nationalarchives.gov.uk>
2026-04-10 05:56:52 +0100

    Bump dependencies
    Lambda has been moved to different subnet but still has ENU attached to old subnet
    
    Force a deploy of the Lambda to detach it from the affected ENI

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object Dependencies {
   lazy val typesafe = "com.typesafe" % "config" % "1.4.6"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.20"
   lazy val circeCore = "io.circe" %% "circe-core" % "0.14.15"
   lazy val circeGeneric = "io.circe" %% "circe-generic" % "0.14.15"
   lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.15"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.7.0"
-  lazy val s3Utils = "uk.gov.nationalarchives" %% "s3-utils" % "0.1.319"
+  lazy val s3Utils = "uk.gov.nationalarchives" %% "s3-utils" % "0.1.323"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "3.0.1"
 }
